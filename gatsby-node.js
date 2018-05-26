ef19898d54f9d398408eb49870e81d91e92dca32
AJ Priola <ajpriola@gmail.com>
2018-05-26 12:13:21 -0700

    Added webpack resolver, for example can now use components/Button instead of ../components/Button

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,16 @@
+const path = require('path');
 /**
  * Implement Gatsby's Node APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
- // You can delete this file if you're not using it
+exports.modifyWebpackConfig = function({config, env}) {
+  config.merge({
+    resolve: {
+      root: path.resolve(__dirname, 'src'),
+      extensions: ['', '.js', '.jsx', '.json'],
+    }
+  });
+  return config;
+}
